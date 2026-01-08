cd85d894a39577c44b7d574bba0d3a5af1185427
kyy1-cyy <iepjohn53@gmail.com>
2026-01-09 05:27:57 +1100

    Fix: Added missing imports in MainActivity (v3.4.1)

--- app/src/main/java/com/squire/vr/MainActivity.java
+++ app/src/main/java/com/squire/vr/MainActivity.java
@@ -1,5 +1,7 @@
 package com.squire.vr;
 
+import android.content.BroadcastReceiver;
+import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
